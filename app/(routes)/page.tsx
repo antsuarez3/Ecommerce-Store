8a5d9f6b1555ecc2807f3ce6b55598c50f90db86
anthonysuarez <antsuarez305@gmail.com>
2023-09-28 18:19:22 -0500

    Create billboard page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,18 @@
-const HomePage = () => {
-  return <div>Home Page</div>
+import getBillboard from '@/actions/get-billboard'
+import Billboard from '@/components/billboard'
+import Container from '@/components/ui/container'
+
+export const revalidation = 0
+
+const HomePage = async () => {
+  const billboard = await getBillboard('96ffffe1-40df-41e5-8df7-95f9e6456ebb')
+  return (
+    <Container>
+      <div className='space-y-10 pb-10'>
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  )
 }
 
 export default HomePage
