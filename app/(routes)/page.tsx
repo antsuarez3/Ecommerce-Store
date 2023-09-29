be2f0dcd32e62aefaece6d5036e0dbf4b2057c91
anthonysuarez <antsuarez305@gmail.com>
2023-09-29 17:49:07 -0500

    Create Product card

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -9,13 +9,14 @@ export const revalidation = 0
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true })
   const billboard = await getBillboard('96ffffe1-40df-41e5-8df7-95f9e6456ebb')
+
   return (
     <Container>
       <div className='space-y-10 pb-10'>
         <Billboard data={billboard} />
-      </div>
-      <div className='flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8'>
-        <ProductList title='Featured Products' items={products} />
+        <div className='flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8'>
+          <ProductList title='Featured Products' items={products} />
+        </div>
       </div>
     </Container>
   )
